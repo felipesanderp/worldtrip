2408e5bf0127d50042dfc1d8dd4a91aa88d5d681
Felipe Sander <felipe.panisson@hotmail.com>
2022-01-06 17:13:48 -0300

    add swiper example to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,13 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 import { theme } from '../styles/theme';
 
+import "swiper/css/bundle";
+import "swiper/css";
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+
+import '../styles/slider.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,13 @@
-import { Flex, Stack } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import SwiperCore, {
+  Navigation,
+  Pagination
+} from 'swiper';
+
+SwiperCore.use([Navigation, Pagination]);
 
 import { Banner } from "../components/Banner";
 import { Header } from "../components/Header";
@@ -12,6 +21,18 @@ export default function Home() {
       <Banner />
     
       <TravelTypes />
+
+      <Swiper
+        // install Swiper modules
+        navigation={true}
+        pagination={true}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
     </Flex>
   )
 }
