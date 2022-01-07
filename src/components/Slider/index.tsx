583f2b5927a531f27c8546634ccf0a4e7b5db58b
Felipe Sander <felipe.panisson@hotmail.com>
2022-01-07 16:34:16 -0300

    add other continents to slider

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -15,7 +15,91 @@ export function Slider() {
         navigation
         pagination={{ clickable: true }}
         style={{width: '100%', flex: '1'}}
-      > 
+      >
+        <SwiperSlide>
+          <Flex
+            w="100%"
+            h="100%"
+            align="center"
+            justify="center"
+            direction="column"
+            bgImage={`/static/north-america.jpg`}
+            bgPosition="100% 30%"
+            bgRepeat="no-repeat"
+            bgSize="cover"
+            textAlign="center"
+          >
+            <Link href="#">
+              <a>
+                <Heading fontSize={["3xl","4xl","5xl"]} color="gray.100" fontWeight="bold">America do Norte</Heading>
+                <Text fontWeight="bold" color="gray.300" fontSize={["0.8rem","1xl", "2xl"]} mt={["2","4"]}>O continente mais antigo</Text>
+              </a>
+            </Link>
+          </Flex>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Flex
+            w="100%"
+            h="100%"
+            align="center"
+            justify="center"
+            direction="column"
+            bgImage={`/static/south-america.jpg`}
+            bgPosition="100% 30%"
+            bgRepeat="no-repeat"
+            bgSize="cover"
+            textAlign="center"
+          >
+            <Link href="#">
+              <a>
+                <Heading fontSize={["3xl","4xl","5xl"]} color="gray.100" fontWeight="bold">America do Sul</Heading>
+                <Text fontWeight="bold" color="gray.300" fontSize={["0.8rem","1xl", "2xl"]} mt={["2","4"]}>O continente mais antigo</Text>
+              </a>
+            </Link>
+          </Flex>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Flex
+            w="100%"
+            h="100%"
+            align="center"
+            justify="center"
+            direction="column"
+            bgImage={`/static/asia.jpg`}
+            bgPosition="100% 30%"
+            bgRepeat="no-repeat"
+            bgSize="cover"
+            textAlign="center"
+          >
+            <Link href="#">
+              <a>
+                <Heading fontSize={["3xl","4xl","5xl"]} color="gray.100" fontWeight="bold">Ásia</Heading>
+                <Text fontWeight="bold" color="gray.300" fontSize={["0.8rem","1xl", "2xl"]} mt={["2","4"]}>O continente mais antigo</Text>
+              </a>
+            </Link>
+          </Flex>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Flex
+            w="100%"
+            h="100%"
+            align="center"
+            justify="center"
+            direction="column"
+            bgImage={`/static/africa.jpg`}
+            bgPosition="100% 30%"
+            bgRepeat="no-repeat"
+            bgSize="cover"
+            textAlign="center"
+          >
+            <Link href="#">
+              <a>
+                <Heading fontSize={["3xl","4xl","5xl"]} color="gray.100" fontWeight="bold">África</Heading>
+                <Text fontWeight="bold" color="gray.300" fontSize={["0.8rem","1xl", "2xl"]} mt={["2","4"]}>O continente mais antigo</Text>
+              </a>
+            </Link>
+          </Flex>
+        </SwiperSlide>
         <SwiperSlide>
           <Flex
             w="100%"
@@ -38,27 +122,7 @@ export function Slider() {
           </Flex>
         </SwiperSlide>
 
-        <SwiperSlide>
-          <Flex
-            w="100%"
-            h="100%"
-            align="center"
-            justify="center"
-            direction="column"
-            bgImage={`/static/banner.png`}
-            bgPosition="100% 30%"
-            bgRepeat="no-repeat"
-            bgSize="cover"
-            textAlign="center"
-          >
-            <Link href="#">
-              <a>
-                <Heading fontSize={["3xl","4xl","5xl"]} color="gray.100" fontWeight="bold">Europa</Heading>
-                <Text fontWeight="bold" color="gray.300" fontSize={["0.8rem","1xl", "2xl"]} mt={["2","4"]}>O continente mais antigo</Text>
-              </a>
-            </Link>
-          </Flex>
-        </SwiperSlide>
+        
 
       </Swiper>
     </Flex>
